069c8311434afafa28fb15dc96fcd10ab2fe769c
sleepy <bbie.eng@gmail.com>
2025-03-23 18:18:06 +0700

    Add Cucumber Spring Configuration With Active profiles dev or sit

--- src/test/kotlin/com/example/book/cucumber/steps/CucumberSpringConfiguration.kt
+++ src/test/kotlin/com/example/book/cucumber/steps/CucumberSpringConfiguration.kt
@@ -2,7 +2,9 @@ package com.example.book.cucumber.steps
 
 import io.cucumber.spring.CucumberContextConfiguration
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @CucumberContextConfiguration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("dev")
 class CucumberSpringConfiguration
